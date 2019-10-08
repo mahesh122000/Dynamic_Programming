4d5eb1d0526beab594dca4306abe6318134a008e
P.Mahesh <46820201+mahesh122000@users.noreply.github.com>
2019-10-08 07:07:53 +0530

    Add files via upload

---
+++ distinct_transformations.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    
+    while(t--)
+    {
+        string s1;
+        string s2;
+        cin>>s1>>s2;
+        
+        int len1 = s1.length();
+        int len2 = s2.length();
+        
+        int dp[len2+1][len1+1];
+        
+        memset(dp,0,sizeof(dp));
+        
+        for(int i=0;i<len1+1;i++)
+        dp[0][i]=1;
+        
+        
+        
+        for(int i=1;i<len2+1;i++)
+        {
+            for(int j=1;j<len1+1;j++)
+            {
+                if(s1[j-1]==s2[i-1])
+                {
+                   dp[i][j]  = dp[i-1][j-1];
+                }
+                
+                dp[i][j] += dp[i][j-1];
+            }
+        }
+        
+        cout<<dp[len2][len1]<<endl;
+        
+    }
+	
+	return 0;
+}
