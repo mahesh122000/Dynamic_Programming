144b50570d89545826f738e2f7e4724294952846
P.Mahesh <46820201+mahesh122000@users.noreply.github.com>
2019-10-16 06:30:15 +0530

    Update distinct_transformations.cpp

--- distinct_transformations.cpp
+++ distinct_transformations.cpp
@@ -37,6 +37,56 @@ int main()
         }
         
         cout<<dp[len2][len1]<<endl;
+	    
+	    
+	    
+// Another approach is:
+	    
+	    
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    
+    while(t--)
+    {
+        string s1;
+        string s2;
+        cin>>s1>>s2;
+        
+        int len1 = s1.length();
+        int len2 = s2.length();
+        
+        int dp[len1+1][len2+1];
+        
+        memset(dp,0,sizeof(dp));
+        
+        for(int i=0;i<len1+1;i++)
+        dp[i][0]=1;
+        
+        
+        
+        for(int i=1;i<len1+1;i++)
+        {
+            for(int j=1;j<len2+1;j++)
+            {
+                if(s1[i-1]==s2[j-1])
+                {
+                   dp[i][j]  = dp[i-1][j-1]+dp[i-1][j];
+                }
+                else
+                  dp[i][j] = dp[i-1][j];
+            }
+        }
+        
+        cout<<dp[len1][len2]<<endl;
+        
+    }
+	
+	return 0;
+}
         
     }
 	
