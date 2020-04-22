2d07fa4281b53bbb5b4b952e914aa91ff3736ac2
P.Mahesh <46820201+mahesh122000@users.noreply.github.com>
2020-04-22 10:16:36 +0530

    Add files via upload

---
+++ Integer_Replacement.java
@@ -0,0 +1,10 @@
+public int integerReplacement(int n) {
+        if(n<=1)
+            return 0;
+        if(n%2==0)
+            return 1+integerReplacement(n/2);
+        else if(n==Integer.MAX_VALUE)
+            return 32;
+        else
+            return 1+Math.min(integerReplacement(n-1),integerReplacement(n+1));
+    }
