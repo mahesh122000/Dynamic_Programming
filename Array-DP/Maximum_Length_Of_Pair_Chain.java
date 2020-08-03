e886e6478d8488b6f4e0b4738c4e6878ce674fd9
P.Mahesh <46820201+mahesh122000@users.noreply.github.com>
2020-08-03 21:37:14 +0530

    Add files via upload

---
+++ Array-DP/Maximum_Length_Of_Pair_Chain.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int findLongestChain(int[][] a) {
+        Arrays.sort(a,(p,q)->p[0]==q[0]?p[1]-q[1]:p[0]-q[0]);
+        int n=a.length;
+        int b[]=new int[n];
+        Arrays.fill(b,1);
+        for(int i=1;i<n;i++)
+        {
+            for(int j=0;j<i;j++)
+            {
+                if(a[i][0]>a[j][1] && b[i]<b[j]+1)
+                    b[i]=b[j]+1;
+            }
+        }
+        int max=0;
+        for(int i=0;i<n;i++)
+            max=Math.max(max,b[i]);
+        return max;
+    }
+}
